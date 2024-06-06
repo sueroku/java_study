9920b88b81b56fcd05aa88f86f3e16369d68e881
sueroku <eraltndusn@gmail.com>
2024-06-06 15:12:35 +0900

    fix

---
+++ src/week3/Baekjoon1264.java
@@ -0,0 +1,18 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Baekjoon1264 {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+
+        bw.flush();
+        bw.close();
+    }
+}
