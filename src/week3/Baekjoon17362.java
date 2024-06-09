ff442c05c92bfdb76fd66c430ebbcb2d166350d1
sueroku <eraltndusn@gmail.com>
2024-06-09 23:20:18 +0900

    0609

--- src/week3/Baekjoon17362.java
+++ src/week3/Baekjoon17362.java
@@ -6,8 +6,16 @@ public class Baekjoon17362 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-
-
-
+        int[] nums = new int[]{1,2,3,4,5,4,3,2};
+        int result=0;
+        int index=0;
+        for(int i=0;i<N; i++){
+            if(index==nums.length){
+                index =0;
+            }
+            result = nums[index];
+            index++;
+        }
+        System.out.println(result);
     }
 }
