6e23535285515562a21e3034bf85c75b5c06b7e4
sueroku <eraltndusn@gmail.com>
2024-06-12 00:56:19 +0900

    home

--- src/week3_binary/Baekjoon1654.java
+++ src/week3_binary/Baekjoon1654.java
@@ -3,7 +3,6 @@ package week3_binary;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Baekjoon1654 {
@@ -13,21 +12,25 @@ public class Baekjoon1654 {
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
         int[] kLength = new int[K];
+        long max = Integer.MIN_VALUE;
+        long min = 0;
+        long mid;
+
         for(int i=0;i<K;i++){
             kLength[i] = Integer.parseInt(br.readLine());
+            max = Math.max(max,kLength[i]);
         }
-        Arrays.sort(kLength);
-        int min = 0;
-        int max = kLength[K-1]+1;
-        int mid;
-        int num =0;
-        while(min<=max){
+
+        max = max +1;
+        long num =0;
+
+        while(min < max){
             mid = (min+max)/2;
             num=0;
             for(int i=0; i<K;i++){
                 num += kLength[i]/mid;
             }
-            if(num<N)max = mid-1;
+            if(num<N) max = mid;
             else min=mid+1;
         }
         System.out.println(min-1);
--- src/week3_binary/Baekjoon2110.java
+++ src/week3_binary/Baekjoon2110.java
@@ -1,10 +1,12 @@
 package week3_binary;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Baekjoon2110 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = sc.nextInt();
         int C = sc.nextInt();
         int[] place = new int[N];
