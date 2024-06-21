f30dc696cdeb73a484b90a86e5fa3b9448aceef9
sueroku <eraltndusn@gmail.com>
2024-06-21 17:36:42 +0900

    11659

--- src/week4/Baekjoon11659.java
+++ src/week4/Baekjoon11659.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -19,6 +20,13 @@ public class Baekjoon11659 {
         for(int i=1;i<n+1;i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
+        int[] arrSum = new int[n+1];
+        arrSum[1] = arr[1];
+        for(int i=2;i<n+1;i++){
+            arrSum[i] = arrSum[i-1] + arr[i];
+        }
+        System.out.println(Arrays.toString(arrSum));
+
         List<List<Integer>> section = new ArrayList<>();
         for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
@@ -26,11 +34,22 @@ public class Baekjoon11659 {
             section.get(i).add(Integer.parseInt(st.nextToken()));
             section.get(i).add(Integer.parseInt(st.nextToken()));
         }
-        int sum = 0;
-        for(int i=0;i<m;i++){
-            int j=0;
-            while ()
+        for(int i=0 ;i<m;i++){
+            int sum = arrSum[section.get(i).get(1)] - arrSum[section.get(i).get(0)-1];
+            System.out.println(sum);
         }
+
+
+//        int sum = 0;
+//        for(int i=0;i<m;i++){
+//            for(int j =section.get(i).get(0) ; j<=section.get(i).get(section.get(i).size()-1);j++){
+//                sum+=arr[j];
+//            }
+//            System.out.println(sum);
+//            sum=0;
+//        }
+
+
 
     }
 }
