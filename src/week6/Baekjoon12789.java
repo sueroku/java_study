84bb4dd982d6799830da8cc61db7d49a0f762bfa
sueroku <eraltndusn@gmail.com>
2024-07-06 17:50:05 +0900

    0706

--- src/week6/Baekjoon12789.java
+++ src/week6/Baekjoon12789.java
@@ -3,6 +3,8 @@ package week6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -10,12 +12,42 @@ public class Baekjoon12789 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] students = new int[n];
+        Queue<Integer> students = new LinkedList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0 ;i<n;i++){
-            students[i] = Integer.parseInt(st.nextToken());
+            students.add(Integer.parseInt(st.nextToken()));
         }
 
-        Stack<Integer> space = new Stack<>();
+        Stack<Integer> wait = new Stack<>();
+
+        int order =1;
+        String check = "";
+        while(true){
+            if(order==n+1){
+                check="Nice";
+                break;
+            }
+            if(!students.isEmpty() && order == students.peek()){
+                students.poll();
+                order++;
+            }
+            if(!wait.isEmpty() && order == wait.peek()){
+                wait.pop();
+                order++;
+            }
+            else if(!students.isEmpty() && students.peek()!=order){
+                wait.add(students.poll());
+            }
+            if(!students.isEmpty() && !wait.isEmpty() && students.peek()>wait.peek() && students.peek()!=order && wait.peek()!=order){
+                check = "Sad";
+                break;
+            }
+//            System.out.println("student");
+//            System.out.println(students);
+//            System.out.println("wait");
+//            System.out.println(wait);
+        }
+
+        System.out.println(check);
     }
 }
